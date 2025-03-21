17024a92a97c45d6bb2db294e3156e50281326ae
dahyeon777 <daaaa0727@gmail.com>
2025-03-21 18:07:42 +0900

    js2_04

---
+++ js/section02/chapter04.js
@@ -0,0 +1,33 @@
+//1. Spread 연선자
+// -> 흩뿌리다, 펼치다라는 뜻
+// -> 객체나 배열에 저장된 여러개의 값을 개별로 흩뿌려주는 역할
+
+let arr1 = [1,2,3];
+let arr2 = [4, ...arr1,5,6]; //... 스트레드 연산자
+
+let obj1 = {
+    a: 1,
+    b: 2,
+};
+let obj2 = {
+    ...obj1,
+    c: 3,
+    d: 4,
+};
+
+function funcA(p1, p2, p3){
+    console.log(p1,p2,p3);
+}
+
+funcA(...arr1);
+
+//2. Rest 매개변수
+//-> Rest는 나머지, 나머지 매개변수
+
+function funcB(one,two,...rest){ //한방에 모든 인수 받아올 수 있음 배열형태로
+    //Spread 연산자가 아니라 rest 매개변수임
+    console.log(rest);
+
+}
+
+funcB(...arr1)
