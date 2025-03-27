706fce40261200275d668d6793640a431b4632c0
dahyeon777 <daaaa0727@gmail.com>
2025-03-28 01:27:55 +0900

    node.js_02

--- section03/src/index.js
+++ section03/src/index.js
@@ -3,7 +3,11 @@
 // import mul from "./math.js";
 import mul, { add, sub } from "./math.js"; //확장자(js 등) 꼭 명시
 
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(mul(2,3));
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2,3));
 
+import randomColor from "randomcolor"; //라이브러리 가져올때는 from뒤에 이름만 명시하면 됨
+
+const color = randomColor();
+console.log(color);
