b2314742a43479a7560f0bdbade878efd9fad2a7
dahyeon777 <daaaa0727@gmail.com>
2025-03-28 01:15:00 +0900

    node.js_01

---
+++ section03/src/index.js
@@ -0,0 +1,9 @@
+// const {add, sub} = require("./math");
+
+// import mul from "./math.js";
+import mul, { add, sub } from "./math.js"; //확장자(js 등) 꼭 명시
+
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(mul(2,3));
+
