a83f1396c5fd815bdd829247f3d4bf6506a7afbf
dahyeon777 <daaaa0727@gmail.com>
2025-03-28 20:40:37 +0900

    react component

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-
-  return (
+function App() { //앱 컴포넌트, 부모 컴포넌트(루트 컴포넌트)
+  return(
     <>
-      <h1>안녕 리액트!</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
-  )
+  );
 }
 
 export default App
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => { //함수 컴포넌트, 첫글자는 대문자, 자식 컴포넌트
+    return(
+      <header>
+        <h1>header</h1>
+      </header>
+    );
+  };
+
+  export default Header;
