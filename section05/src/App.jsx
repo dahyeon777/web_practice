0336070c428e7e0a445f8df12b83de019fa1c3b7
dahyeon777 <daaaa0727@gmail.com>
2025-04-02 17:44:29 +0900

    state

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,27 +1,32 @@
 import './App.css'
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Button from './components/Button';
+import { useState } from 'react';
 
-function App() { //앱 컴포넌트, 부모 컴포넌트(루트 컴포넌트)
+function App() { 
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  }
-
-  return(
-    <>
-      <Button {...buttonProps}/>
-      <Button text={"카페"}/>
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
-    </>
+  return (
+  <>
+  <div>
+    <h1>{light}</h1>
+    <button 
+      onClick={() => {
+      setLight(light === 'ON' ? "OFF":"ON");
+    }}>
+      {light === "ON" ? "끄기" : "켜기"}
+    </button>
+  </div>
+  <div>
+    <h1>{count}</h1>
+    <button 
+      onClick={()=>{
+      setCount(count + 1);
+    }}
+    >
+      +
+    </button>
+    </div>
+  </>
   );
 }
 
